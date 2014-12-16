79ef3d04c5891b5e3e9175b5f4af47d4848a6214
Jose Vergara <jmvergar@gmail.com>
2014-12-16 00:38:30 -0500

    cris is magic

--- node2.js
+++ node2.js
@@ -1,9 +1,9 @@
 var path = require('path');
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
+var server = require('http').Server(app);
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(8080);
+server.listen(8080);
 
 // Using the filesystem module
 
